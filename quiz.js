45c3b109c12a42d806235ba58606a9096f7e2beb
AndreiPotemkin <Andrei.Potemkin@pfpsoft.com>
2014-06-14 23:38:25 -0500

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -170,7 +170,7 @@ $('#quiz').hide();
 
 function configureImagePickers() {
   //Configure ImagePickers
-  jQuery(".image-picker").imagepicker({
+  Sizzle(".image-picker").imagepicker({
                                           show_label:  true,
                                           clicked: clickedEvent
                                           });
